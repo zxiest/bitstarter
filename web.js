7d257fb91aed3b5445d61d7388768ded3e262d2a
Ubuntu <ubuntu@ip-10-202-30-117.ec2.internal>
2013-07-11 21:08:31 +0000

    modified index to contain twitter bootstrap and basic style

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
   var buf = fs.readFileSync("index.html");
   response.send(buf.toString());
